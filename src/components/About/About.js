be2d510ac5d1ed3a110202331301d7a95a28c931
KaziMdMehediHasan <=>
2022-01-06 23:34:45 +0600

    about page added

---
+++ src/components/About/About.js
@@ -0,0 +1,90 @@
+import React from "react";
+import "./About.css";
+
+const About = () => {
+  return (
+    <div className="about">
+      <div className="about-container">
+        <div className="half">
+          <h1>About Us</h1>
+          <p>
+            <b>AutoRepublic</b> is a dream that turned into a tech based startup, a
+            business with a great purpose to create positive impacts in peoples'
+            lives. <b>AutoRepublic</b> is missioned to
+            bring convenience in the vehicle service delivery for the general
+            people in Bangladesh.
+          </p>
+          <h4>Background</h4>
+          <hr />
+          <p>
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quas qui quo necessitatibus quam praesentium eligendi consectetur sed cupiditate aut ullam ea dicta modi placeat hic nisi voluptate, quisquam in numquam ex velit. Optio impedit facilis dignissimos sint eos quidem? Ad culpa sapiente hic, adipisci at numquam asperiores quod. Reiciendis vero consectetur minima rem. Perspiciatis, rerum maxime vel impedit debitis odit ipsam recusandae accusantium mollitia quibusdam. Delectus aperiam excepturi, architecto, expedita numquam laborum, sed nostrum voluptates porro non molestiae! Cumque quibusdam dignissimos officia adipisci quidem ullam fugiat reiciendis exercitationem ut itaque? Iusto necessitatibus quaerat dolorum quo natus minus possimus pariatur! Tempore!
+          </p>
+          <h4>Evolution into a Service Platform</h4>
+          <hr />
+          <p>
+            The idea of <b>AutoRepublic</b> Lorem ipsum dolor sit amet consectetur adipisicing elit. Inventore magni porro, expedita iste deserunt, veritatis vel voluptas illo et dicta accusantium consectetur quos earum voluptatibus similique sit eius quis distinctio maxime labore minus. Incidunt, ut. Nobis laboriosam, molestias qui quae, rem sint pariatur, vitae molestiae dolorem asperiores modi iste obcaecati. Quisquam exercitationem dignissimos dolore nam, eaque tempore quibusdam possimus at beatae nemo harum vero magnam dolorem. Consectetur rem vel explicabo. <b>AutoRepublic</b> Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus ex libero iste consequuntur nesciunt culpa impedit molestiae enim repudiandae animi a dicta, modi sunt totam eius distinctio ipsa, quo expedita suscipit deleniti reprehenderit magnam repellat nihil. Omnis repellendus tenetur qui.
+            <b>AutoRepublic</b> Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis maiores asperiores ea quibusdam numquam aut temporibus, ratione labore doloremque repudiandae corporis quae odio quo explicabo odit soluta veniam modi magni! Repudiandae hic architecto facere rem a accusamus quibusdam quos placeat.
+          </p>
+          <h4>Changing Mindsets</h4>
+          <hr />
+          <p>
+            <b>AutoRepublic</b> Lorem ipsum dolor sit amet conaims to drive two very important behavioral changes
+            that can make approaching treatment faster and better. In general, a
+            common person in our country is quite unwilling to see a proper
+            physician for his or her illness, until it gets too bad. People try
+            out different suggestions from friends around them or conduct a self
+            treatment based on other's experiences. This is mainly due to
+            serious lack of awareness about the signs, symptoms, consequences of
+            diseases and lack of proper guidance. Secondly, when people want to
+            approach treatment, they commonly get into the trouble of finding a
+            right doctor. In most cases, they head to Dhaka or other big cities
+            and ask their relatives or friends for help. Unfortunately, those
+            relatives or friends typically know only a few doctor names. Those
+            are generally the busiest doctors who are well known at the national
+            level. Certainly, taking an appointment of those doctors are quite a
+            hassle. One may need to wait even two-three months to consult the
+            doctor. This is again mainly because, there is not enough awareness
+            about the availability of many qualified doctors across the country
+            who could give attention and treat the patient earlier than the time
+            he/she will wait for. These result in unnecessary delays aggravating
+            the condition of the disease. <b>Medify</b> through its social
+            network platform and many other means is continuously engaging to
+            build the awareness among people to help these improve.
+          </p>
+          <h4>Expansion with Impact</h4>
+          <hr />
+          <p>
+            If we are to truly improve this with the right impact, we must focus
+            on the people at the rural or in small cities. Providing and
+            extending this service to the rural people of Bangladesh requires a
+            lot of resources and activities. And, it isn't easy with the limited
+            resources.
+          </p>
+          <h4>Present Operation</h4>
+          <hr />
+          <p>
+            Since the official launching on 31st October 2015, <b>Medify</b> is
+            operational with full capacity in regard to providing appointments
+            from doctors around the country. The service hosts information and
+            appointment facilities for more than 9615 doctors from 556
+            consultation centers in 63 districts. We have served a few thousand
+            patients so far and have been able to bring smiles on their faces.
+          </p>
+          <h4>The Future</h4>
+          <hr />
+          <p>
+            At <b>Medify</b>, we are confident to make a difference in how
+            people of Bangladesh approach healthcare services. We have plans to
+            integrate many other related services with the doctors' appointment
+            in the platform in coming days. The investment from BD Venture has
+            given the initiative more speed. Now, it's all about execution of
+            the dreams to bring goodness out for everyone in the country.
+          </p>
+        </div>
+        <div className="half"></div>
+      </div>
+    </div>
+  );
+};
+
+export default About;
